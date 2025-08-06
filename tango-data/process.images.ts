ad82b99acacb2afb0de45677e9a638e8fe485206
wswebcreation <wim@wswebcreation.nl>
2025-08-06 09:29:55 +0200

    chore: add 101-125

--- tango-data/process.images.ts
+++ tango-data/process.images.ts
@@ -36,7 +36,7 @@ const prefilledImagesFolder = `${processedImagesFolder}/5. prefilled`;
 // Option 2: Process all files in thumbnails folder
 // const files = readdirSync('tango-data/thumbnails/').map(file => `tango-data/thumbnails/${file}`);
 // Option 3: Process a range of puzzle numbers (e.g., puzzles 20-30)
-const files = generatePuzzleFileRange(76, 100);
+const files = generatePuzzleFileRange(101, 125);
 
 async function processImages(): Promise<void> {
     removeDirectory(processedImagesFolder);
